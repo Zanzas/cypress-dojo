1e117df8e762c270ed5ef5c76c8b5ac007ba91a8
Youssef <you.benhaddou@gmail.com>
2019-11-27 14:51:05 +0100

    remove beforeeach

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -6,23 +6,26 @@ const url = 'https://www.pokebip.com/pokedex/6G_liste_des_pokemon.html';
 describe('Home Page', () => {
   const NUMBER_OF_POKEMONS = 721;
 
-  beforeEach(() => {
+  // 1.  Use beforeEach and visit url in it
+  it('it should provide all pokemons table', () => {
     // Given I visit the home page
     cy.visit(url)
-  })
 
-  it('it should provide all pokemons table', () => {
     // Then I should see the title     
     cy.get('h1')
       .should('contain', 'Liste des pokémon')
 
     // And a list of all 721 pokemons
-    cy.get('#g6_liste_pkmn tr')
+    cy.get('#g6_liste_pkmn').as('pokemonList')
+    cy.get('@pokemonList').find('tr')
       .should('have.length', NUMBER_OF_POKEMONS+1) // +1 for the table header
 
   })
 
   it('it should fiter pokemon', () => {
+    // Given I visit the home page
+    cy.visit(url)
+
     // When I filter
     cy.get(`input ~ img[alt='Dragon']`).prev().check()
     cy.get(`input ~ img[alt='Spectre']`).prev().check()
@@ -36,25 +39,30 @@ describe('Home Page', () => {
 
   })
 
-  // Create a Command to simplify check type
+  // 1. Create a Command to simplify check type
   it('it should filter pokemon by type and attack', () => {
 
   })
 
-  // Create a Command to simplify check type
   it('it should filter pokemon by type and attack', () => {
     
   })
 
-  it('it should be to visit detail page of second pokemon', () => {
-    cy.get('#g6_liste_pkmn tr:eq(2) a')
+  it('it should be able to visit detail page of second pokemon', () => {
+    // Given I visit the home page
+    cy.visit(url)
+
+    // When I click on second pokemon
+    cy.get('#g6_liste_pkmn tr').eq(2).find('a')
       .click()
 
+    // Then i redirected to the correct path
     cy.location('pathname').should('include', 'Herbizarre')
 
   })
 
-  it('it should be to visit detail page and go back to home', () => {
+  // use go function
+  it('it should be able to visit detail page and go back to home', () => {
     
   })
 
